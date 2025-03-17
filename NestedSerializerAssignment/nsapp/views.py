7f669340760a062566308eb1d7c4c3e5f90dfa00
Nitesh Kumar <nkniteshstar@gmail.com>
2025-03-17 23:53:38 +0530

    Adding authentication and permission in NestedSerializerAssignment

--- NestedSerializerAssignment/nsapp/views.py
+++ NestedSerializerAssignment/nsapp/views.py
@@ -2,11 +2,16 @@ from rest_framework import generics
 from .models import Customer, Order
 from .serializers import CustomerSerializer, OrderSerializer
 
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
+
 # Create your views here.
 
 class CustomerListCreate(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 
 class CustomerRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
