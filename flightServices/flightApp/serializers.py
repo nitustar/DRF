c8b19bf545002cfc58bca088e13dfe5c2bafb4c9
Nitesh Kumar <nkniteshstar@gmail.com>
2025-03-19 23:32:16 +0530

    Adding different ways to create validations

--- flightServices/flightApp/serializers.py
+++ flightServices/flightApp/serializers.py
@@ -3,15 +3,20 @@ from .models import Flight, Passenger, Reservation
 
 import re
 
+def validate_data(data):                    # Custom validator function -> that can be used for validating data fields for any serializer
+    print(data)
+    return data
+
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+        validators = [validate_data]
 
-    def validate_flight_number(self, flight_number):
-        if re.match("^[a-zA-Z0-9]*$", flight_number)==None:
+    def validate(self, data):           # Custom validator function -> that can be used for validate multiple data fields
+        if re.match("^[a-zA-Z0-9]*$", data['flight_number'])==None:
             raise serializers.ValidationError("Invalid flight number. Make sure it contains only alphanumeric characters.")
-        return flight_number
+        return data
 
 class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
